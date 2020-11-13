d6d753c855fcc58b969c5a08bfa79e1a5d932398
houtanfarin <houtanf@gmail.com>
2020-11-12 22:16:16 -0800

    built Currency Converter

---
+++ src/Currency.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Currency(props) {
+  const {
+    currencyOptions,
+    selectedCurrency,
+    onChangeCurrency,
+    amount,
+    onChangeAmount,
+  } = props;
+
+  return (
+    <div>
+      <input
+        className="input"
+        type="number"
+        value={amount}
+        onChange={onChangeAmount}
+      />
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
+        {currencyOptions.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
